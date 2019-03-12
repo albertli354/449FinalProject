905779f2d8d929ca19e2eb74d87b4d0f980bde51
Keegan Hanes <keeganhanes4@gmail.com>
2019-03-12 16:42:16 -0700

    Add spotify quick start project

---
+++ spotify-ios-quick-start/spotify-ios-quick-start-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  spotify-ios-quick-start-Bridging-Header.h
+//  spotify-ios-quick-start
+//
+//  Created by Keegan Hanes on 3/9/19.
+//  Copyright © 2019 Keegan Hanes. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
---
+++ spotify-ios-quick-start/spotify-ios-quick-start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotify-ios-quick-start
+//
+//  Created by Keegan Hanes on 3/9/19.
+//  Copyright © 2019 Keegan Hanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
