ecf11ea03e77f0af349f07106123530dfff3099f
Keegan Hanes <keeganhanes4@gmail.com>
2019-03-15 16:43:20 -0700

    First commit

---
+++ spotify-quick-start-3/spotify-quick-start-3-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  spotify-quick-start-3-Bridging-Header.h
+//  spotify-quick-start-3
+//
+//  Created by Keegan Hanes on 3/15/19.
+//  Copyright © 2019 Keegan Hanes. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
